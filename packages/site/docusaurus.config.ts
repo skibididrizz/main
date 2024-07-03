b3e2939aea7a0f50039e8fda68af735e5dfc2799
Justin Spears <speajus@gmail.com>
2024-07-03 10:57:55 -0700

    fix baseurl

--- packages/site/docusaurus.config.ts
+++ packages/site/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://skibididrizz.github.com/main',
+  url: 'https://skibididrizz.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/main/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
