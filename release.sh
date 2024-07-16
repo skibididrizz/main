f28b913a43af64d51d03fe426742182aac133706
Justin Spears <speajus@gmail.com>
2024-07-16 12:31:06 -0700

    update release script

--- release.sh
+++ release.sh
@@ -4,14 +4,25 @@ VERSION=$1
 [ -z "$npm_package_version" ] && echo "error: npm_package_version not set" && exit 1
 
 case $VERSION; in
-  patch) VERSION=$(echo $npm_package_version | awk -F. '{print $1"."$2"."$3+1}');;
-  minor) VERSION=$(echo $npm_package_version | awk -F. '{print $1"."$2+1".0"}');;
-  major) VERSION=$(echo $npm_package_version | awk -F. '{print $1+1".0.0"}');;
+  patch) VERSION=$(echo $npm_package_version | IFS="." read a m p; echo "$a.$m.$((p+1))");;
+  minor) VERSION=$(echo $npm_package_version | IFS="." read a m p; echo "$a.$((m+1)).0");;
+  major) VERSION=$(echo $npm_package_version | IFS="." read a m p; echo "$((a+1)).0.0");;
 esac
 
-
+confirm() {
+    echo -n "$1'?[yN] "
+    read -sk REPLY
+    case $REPLY in
+        [Yy]) echo $2;;
+        [Nn]) exit 1;;
+        *) confirm "\n$1" $2;;
+    esac
+}
+confirm "bump version from '$npm_package_version' to '$VERSION'" "new version $VERSION"
 
 yarn workspaces foreach -A version ${VERSION} && \
-git commit -am \"v${VERSION}\" && git tag v${VERSION} && \
-echo "bumped to version '${VERSION}' from '$npm_package_version'" && \
-echo "do not forget to 'git push --tags origin main' to update docs and release new version"
+ git commit -am \"v${VERSION}\" && git tag v${VERSION} && \
+ echo "bumped to version '${VERSION}' from '$npm_package_version'" 
+
+confirm "push tags to release a new version" "pushing"
+git push origin main --tags
